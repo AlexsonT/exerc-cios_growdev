b4c621daf957f6909859216567b7c32b9c43fbf7
Alexson Theobald <alexson.bass@gmail.com>
2022-06-07 18:59:44 -0300

    complemento

--- exercicios_300522/03_n_para_e.py
+++ exercicios_300522/03_n_para_e.py
@@ -2,13 +2,27 @@
 #E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + ... + 1 / N!
 
 
-adicao = soma = n = 0
-while adicao != 2:
-    print('-*'*25)
-    n = int(input('Insira um número inteiro: '))
-    print(' [1] Adicionar + valor\n [2] Parar')
-    adicao = int(input('Insira uma opção: '))
-    soma += n
-print('='*50)    
-print(f'O valor de E é {soma}')   
-print('='*50)
+# adicao = soma = n = 0
+# while adicao != 2:
+#     print('-*'*25)
+#     n = int(input('Insira um número inteiro: '))
+#     print(' [1] Adicionar + valor\n [2] Parar')
+#     adicao = int(input('Insira uma opção: '))
+#     soma += n
+# print('='*50)    
+# print(f'O valor de E é {soma}')   
+# print('='*50)
+
+n = int(input("Informe um número: "))
+
+valor_e = 1
+
+for i in range(1, n + 1):
+
+    fatorial = 1
+    for j in range(i, 1, -1):
+        fatorial = fatorial * j
+    
+    valor_e += 1 / fatorial
+
+print(f"E: {valor_e}")
